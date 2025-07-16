266d14204d33e1e2a5a323033c5612ae88099cea
martin-mugisha <2021bce013@std.must.ac.ug>
2025-07-16 21:19:11 +0300

    close to final design

--- little_money/little_money/settings.py
+++ little_money/little_money/settings.py
@@ -121,6 +121,11 @@ DATABASES = {
     }
 }
 
+CSRF_COOKIE_DOMAIN = ".mangupay.tech"
+SESSION_COOKIE_DOMAIN = ".mangupay.tech"
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
